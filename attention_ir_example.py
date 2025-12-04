b197cce14286c62ffa6fc32be2838c8d9bd5d3c9
Da1sypetals <da1sypetals.iota@gmail.com>
2025-12-04 22:12:27 +0800

    Fix kjt loading and transpose in flash attention v2

--- attention_ir_example.py
+++ attention_ir_example.py
@@ -30,8 +30,10 @@ def flash_attention_forward_v2(
 ):
     ib = ct.bid(0)
     ih = ct.bid(1)
+    i_qblock = ct.bid(2)
+
     Tc = k.shape[0] // bc
-    qi = ct.load(q, index=(ib, ih, ct.bid(2), 0), shape=(1, 1, br, hidden_size))
+    qi = ct.load(q, index=(ib, ih, i_qblock, 0), shape=(1, 1, br, hidden_size))
 
     qi = ct.reshape(qi, (br, hidden_size))
 
@@ -40,12 +42,11 @@ def flash_attention_forward_v2(
     mi = ct.full((br, 1), -1e10, dtype=q.dtype)
 
     for j in range(0, Tc):
-        # kj = ct.load(k, index=(0, j), shape=(hidden_size, bc), order="F")
-        # vj = ct.load(v, index=(j, 0), shape=(bc, hidden_size))
-        kj = ct.load(k, index=(ib, ih, 0, j), shape=(1, 1, hidden_size, bc), order="F")
+        kjt = ct.load(k, index=(ib, ih, j, 0), shape=(1, 1, bc, hidden_size))
         vj = ct.load(v, index=(ib, ih, j, 0), shape=(1, 1, bc, hidden_size))
-        kj = ct.reshape(kj, (hidden_size, bc))
+        kjt = ct.reshape(kjt, (bc, hidden_size))
         vj = ct.reshape(vj, (bc, hidden_size))
+        kj = ct.transpose(kjt)
         sij = ct.matmul(qi, kj) / hidden_size**0.5
         mij = ct.max(sij, axis=-1, keepdims=True)
         mi_mij = ct.cat((mi, mij), axis=-1)
@@ -59,7 +60,7 @@ def flash_attention_forward_v2(
         mi = mi_new
 
     oi = oi / li
-    ct.store(out, index=(ib, ih, ct.bid(2), 0), tile=oi)
+    ct.store(out, index=(ib, ih, i_qblock, 0), tile=oi)
 
 
 def main():
